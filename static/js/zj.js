dd32fdd8431dd0ebfcc1abf706124a09629402e3
Traipd <38999466+Traipd@users.noreply.github.com>
2018-06-03 18:37:52 +0800

    界面功能优化0.1
    界面功能优化

---
+++ static/js/zj.js
@@ -0,0 +1,26 @@
+﻿function makeDisabledByClass(a,c) {//修改(父标签ID，下‘class名’)的Disabled
+    var oUl=document.getElementById(a);
+    var aBox=getByClass(oUl, c);
+
+    for(var i=0;i<aBox.length;i++){
+        if(aBox[i].disabled==false )/*或使用.readOnly .disabled*/
+        { aBox[i].disabled= true;}
+        else
+        {aBox[i].disabled=false;}
+    }
+
+}
+
+function getByClass(oParent, sClass){//通过Class获取标签列表(父标签，‘class名’)
+    var aResult=[];
+    var aEle=oParent.getElementsByTagName('*');
+
+    for(var i=0;i<aEle.length;i++){
+        if(aEle[i].className==sClass)
+        {
+            aResult.push(aEle[i]);
+        }
+    }
+
+    return aResult;
+}
